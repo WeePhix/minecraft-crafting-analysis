1c6ee331576bff24cc3f4a473d4a32e88e216902
WeePhix <evanjasaromih@gmail.com>
2024-07-23 18:31:47 +0200

    Ločevanje vrst predmetov (WIP)

--- program.py
+++ program.py
@@ -5,20 +5,24 @@ from selenium.webdriver.common.by import By
 web_url = "https://minecraft.wiki/w/Crafting"
 html_filename = "page.html"
 csv_filename = "data.csv"
+section_file_name = "section"
 directory = "data"
 show_button_class = "jslink"
 
+section_title = '<table class="wikitable collapsible sortable jquery-tablesorter" data-description="Crafting recipes">'
 
 driver = webdriver.Chrome()
 
 
 def get_html_str(url: str) -> str:
     driver.get(url)
-    driver.implicitly_wait(5)
+    # Instead of waiting 5 seconds every time, find a way to continue the code when the page is loaded
+    driver.implicitly_wait(1)
     buttons = driver.find_elements(By.CLASS_NAME, show_button_class)
     print(buttons)
     for i in range(11):
         buttons[i].click()
+        driver.implicitly_wait(1)
     return driver.page_source
 
 
@@ -30,9 +34,26 @@ def save_str_to_html(text: str, filename: str, directory: str) -> None:
     return
 
 
+def separate_sections(text: str, pattern: str, number_of_sections: int) -> list:
+    index = 0
+    last_index = 0
+    sections = []
+    for i in range(number_of_sections):
+        index = text.index(pattern)
+        sections.append(text[last_index:index])
+        text = text[index:]
+        last_index = index
+    return sections[1:]
+
+
+def save_sections_to_files(filename: str, directory: str):
+    sections = separate_sections(html, section_title, 11)
+    for i, section in enumerate(sections):
+        save_str_to_html(section, filename + str(i) + ".txt", directory)
+
+
 if __name__ == "__main__":
     html = get_html_str(web_url)
-    save_str_to_html(html)
-
-
+    save_str_to_html(html, html_filename, directory)
+    save_sections_to_files(section_file_name, directory)
 driver.close()

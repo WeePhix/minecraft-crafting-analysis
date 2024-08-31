c971ed8278829167e9df13ba71435544c00507f8
WeePhix <evanjasaromih@gmail.com>
2024-08-31 20:12:22 +0200

    https://github.com/WeePhix/minecraft-crafting-analysis Uvedel podporo za spošne materiale (vrste lesa ipd.)

--- program.py
+++ program.py
@@ -2,7 +2,7 @@ import csv, os, re
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-download_from_web = False
+download_from_web = True
 
 web_url = "https://minecraft.wiki/w/Crafting"
 show_button_class = "jslink"
@@ -22,14 +22,15 @@ PATTERN_item_name = r'^(.*?)"'
 PATTERN_item_recipe = r'<span class="mcui-input">(.*?)<span class="mcui-arrow">'    
 PATTERN_item_yield = r'">([0-9]+)</span></a></span></span></span></span></div></td>'
 PATTERN_item_description = r'</div></td><td>(.*?)$'
-# groups follow like this: item name, recipe layout, recipe yield, description
 
-PATTERN_row = r'<span class="mcui-row">(?s:.*?</span>)</span>'
-PATTERN_slot = r'<span class="invslot">(?s:.*?)</span>'
-PATTERN_full_slot = r'<span class="invslot-item invslot-item-image">?s:.*?title="(?s:.*?)"><img alt='
-
+PATTERN_slot = r'<span class="invslot.*?">(.*?)</span>'
+PATTERN_full_slot = r'<span class="invslot-item invslot-item-image">.*?title="(.*?)"><img alt='
+PATTERN_changing_slot = r'<span class="invslot animated animated-lazyloaded">.*?<a href="/w/(.*?)">.*?</a>'
 
 DESCRIPTIONS_NOJAVA = ['"This statement only applies to Bedrock Edition"', '"This statement only applies to Bedrock Edition and Minecraft Education"']
+GENERAL_items = {r'[Spruce|Dark_Oak|Oak|Birch|Jungle|Warped|Crimson|Cherry|Acacia|Mangrove]': '',
+                 r'Bamboo_Planks': 'Planks', r'[Oxidized|Weathered|Exposed]': '',
+                 r'[White|Light_Gray|Gray|Black|Brown|Red|Orange|Yellow|Lime|Green|Cyan|Light_Blue|Blue|Purple|Magenta|Pink]': ''}
 
 item_dict = {}
 # key is the name of the item, value is the corresponding Item object
@@ -41,14 +42,14 @@ class Item():
         self.section = section
         self.recipes = set()
     
-    def new_recipe(self, recipe: dict) -> None:
+    def new_recipe(self, recipe: dict, ryield: int) -> None:
         out = ''
         keys = list(recipe.keys())
         keys.sort()
         for mat in keys:
             if mat != "yield":
                 out += str(recipe[mat]) + mat
-        out += str(recipe["yield"])
+        out += str(ryield)
         self.recipes.add(out)
     
     def __str__(self) -> str:
@@ -71,9 +72,10 @@ def save_url_to_file(url: str=web_url, show_class:str=show_button_class, filenam
     os.makedirs(directory, exist_ok=True)
     path = os.path.join(directory, filename)
     with open(path, 'w', encoding='utf-8') as file_out:
-        file_out.write(re.sub("\n", "", driver.page_source))
+        no_nl = re.sub("\n", "", driver.page_source)
+        file_out.write(re.sub(r'<span class="mw-headline" id="Removed_recipes">Removed recipes</span>.*', '', no_nl))
         driver.implicitly_wait(2)
-    driver.close()
+    driver.quit()
     return
 
 
@@ -88,54 +90,68 @@ def save_sections_to_files(html_fname: str=FILENAME_html, section_titles: list=s
 
 
 def distinguish_items_in_section(section_fname: str, directory: str=directory, item_pattern: str=PATTERN_item, no_java: str=DESCRIPTIONS_NOJAVA):
-    print("checking through section: " + section_fname)
-    path = os.path.join(directory, section_fname + ".txt")
-    with open(path, "r", encoding="utf8") as file:
+    fpath = os.path.join(directory, section_fname + ".txt")
+    with open(fpath, "r", encoding="utf8") as file:
         section = file.read()
-        print("file read")
     
     items = re.findall(item_pattern, section)
-    print("items in section: " + str(len(items)))
     for item in items:
         name = re.search(PATTERN_item_name, item).group(1)
         recipe = re.search(PATTERN_item_recipe, item).group(1)
         ryield = re.search(PATTERN_item_yield, item)
+        desc = re.search(PATTERN_item_description, item).group(1)
         if ryield:
             ryield = ryield.group(1)
         else:
             ryield = 1
-        desc = re.search(PATTERN_item_description, item).group(1)
-        save_item_to_class(name, recipe, ryield, desc ,section_fname)
+        
+        save_item_to_class(name, recipe, ryield, section_fname)
 
 
-def save_item_to_class(item_name:str, item_html: str, recipe_yield: str, item_section: str, row_pattern:str=PATTERN_row, slot_pattern: str=PATTERN_slot, full_pattern: str=PATTERN_full_slot, item_dict: dict=item_dict) -> None:
-    print("saving recipe of " + item_name)
+def save_item_to_class(item_name:str, item_html: str, recipe_yield: str, item_section: str, slot_pattern: str=PATTERN_slot, changing_pattern: str=PATTERN_changing_slot, full_pattern: str=PATTERN_full_slot, item_dict: dict=item_dict) -> None:
     recipe = {}
-    for row in re.findall(row_pattern, item_html):
-        # checks each slot in the recipe. If it is not empty, the crafting material is added to the recipe dict
-        for slot in re.finditer(slot_pattern, row):
-            if slot.group(1) == '':
-                continue
+    print(item_name)
+    
+    for slot in re.findall(slot_pattern, item_html, re.DOTALL):
+        mat = re.search(full_pattern, slot)
+        if mat:
+            if mat.group(1) in recipe.keys():
+                recipe[mat.group(1)] += 1
             else:
-                mat_name = re.search(full_pattern, slot.group(1)).group(1)
-                if mat_name not in recipe.keys():
-                    recipe[mat_name] = 0
-                recipe[mat_name] += 1
+                recipe[mat.group(1)] = 1
+    
+    for changing_slot in re.findall(changing_pattern, item_html, re.DOTALL):
+        try:
+            specific_mat = changing_slot.group(1)
+        except:
+            specific_mat = changing_slot
+        for specific in GENERAL_items.keys():
+            if re.search(specific, specific_mat):
+                mat = re.sub(specific, GENERAL_items[specific], specific_mat)
+                break
+        
+        if mat in recipe.keys():
+            recipe[mat] += 1
+        else:
+            recipe[mat] = 1
+            
+            
     if recipe_yield == "":
-        recipe["yield"] = 1
+        recipe_yield = 1
     else:
-        recipe["yield"] = int(recipe_yield)
+        recipe_yield = int(recipe_yield)
+    
+    print(item_section, item_name, recipe, recipe_yield)
     
     if item_name not in item_dict.keys():
         item_dict[item_name] = Item(item_name, item_section)
-    item_dict[item_name].new_recipe(recipe)
+    item_dict[item_name].new_recipe(recipe, recipe_yield)
 
 
 def save_items_to_file(item_dict: dict = item_dict, directory: str = directory, file_name = FILENAME_items):
     path = os.path.join(directory, file_name)
     out = ''
     for item in item_dict.values():
-        print("saving to file: " + item.name)
         out += str(item) + "\n#" * 2 + "\n"
     with open(path, "w", encoding="utf8") as file:
         file.write(out)
@@ -145,6 +161,7 @@ if __name__ == "__main__":
     if download_from_web:
         save_url_to_file()
         save_sections_to_files()
+    
     for section in section_titles:
         distinguish_items_in_section(section)
     save_items_to_file()
